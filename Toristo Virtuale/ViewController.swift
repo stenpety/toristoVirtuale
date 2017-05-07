3cc9f766178daadf38c6e9b39ef9933c9211a731
Petr Stenin <petr.stenin@me.com>
2017-05-07 15:16:11 +0300

    Initial commit. App icon added. Project groups created.

---
+++ Toristo Virtuale/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Toristo Virtuale
+//
+//  Created by Petr Stenin on 07/05/2017.
+//  Copyright © 2017 Petr Stenin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
