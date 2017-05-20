981b7c040021d24a880d4f022ac7e75748bf5b6f
Petr Stenin <petr.stenin@me.com>
2017-05-20 14:48:25 +0400

    Custom class for Collection item. Collection VC setup started (flow layout done)

--- Toristo Virtuale/PhotoAlbumViewController.swift
+++ Toristo Virtuale/PhotoAlbumViewController.swift
@@ -12,15 +12,34 @@ import MapKit
 class PhotoAlbumViewController: UICollectionViewController {
     
     // MARK: Properties
+    var numberOfCellsInRow = Constants.initialNumberOfCellsInRow // Set to initial value 3
     
+    
+    // MARK: Outlets
     @IBOutlet weak var auxMapView: MKMapView!
     @IBOutlet weak var locationNameLabel: UILabel!
     @IBOutlet weak var photoAlbumCollection: UICollectionView!
+    @IBOutlet weak var photoAlbumFlowLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var newCollectionButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    // MARK: Auxiliary procedures
+    func setupFlowLayout() {
+        // Setup Inter-item & line spacing to defaults
+        photoAlbumFlowLayout.minimumInteritemSpacing = Constants.collectionMinInteritemSpace
+        photoAlbumFlowLayout.minimumLineSpacing = Constants.collectionMinLineSpace
+        
+        // Set item width with respect to the number of items in row
+        let dimension = ((photoAlbumCollection?.frame.size.width)! - CGFloat(numberOfCellsInRow - 1) * Constants.collectionMinInteritemSpace) / CGFloat(numberOfCellsInRow)
+        photoAlbumFlowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
 }
